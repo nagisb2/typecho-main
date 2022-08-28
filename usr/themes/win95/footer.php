7045e840c071a7989bd560d513ece8f938b3dffa
AB2 <2181890001@qq.com>
2022-08-29 03:14:12 +0800

    2022-8-29

---
+++ usr/themes/win95/footer.php
@@ -0,0 +1,14 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+
+
+	<script src="<?php $this->options->themeUrl('assets/js/001.js'); ?>"></script>
+	<script src="<?php $this->options->themeUrl('assets/js/002.js'); ?>"></script>
+	<div class="footer">
+		<p>Typecho因你而荣耀 | Theme by <a href="https://www.vpsmm.com/" target="_blank">win95</a></p>
+		<!--尊重作者，保留版权信息，如果强烈需要去掉，希望能捐赠作者一杯咖啡，捐赠地址：https://www.vpsmm.com/pay.shtml 谢谢-->
+	</div>
+
+
+<?php $this->footer(); ?>
+</body>
+</html>
